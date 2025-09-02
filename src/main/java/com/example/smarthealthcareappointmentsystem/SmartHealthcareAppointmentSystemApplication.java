9e7dba9cd1032795053bd82ab50087ce4653f687
Kareem.AlQutob <kareemalqutob2003@gmail.com>
2025-09-02 09:51:01 +0300

    init

---
+++ src/main/java/com/example/smarthealthcareappointmentsystem/SmartHealthcareAppointmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.smarthealthcareappointmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHealthcareAppointmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHealthcareAppointmentSystemApplication.class, args);
+    }
+
+}
