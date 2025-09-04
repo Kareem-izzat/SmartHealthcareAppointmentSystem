312fceb985d9dacc31c7220cfccda8b8aefcf015
Kareem Al-Qutob <97916953+Kareem-izzat@users.noreply.github.com>
2025-09-04 18:23:26 +0300

    DoctorController

---
+++ src/main/java/com/example/smarthealthcareappointmentsystem/controller/DoctorController.java
@@ -0,0 +1,49 @@
+package com.example.smarthealthcareappointmentsystem.controller;
+
+import com.example.smarthealthcareappointmentsystem.DTO.AppointmentDto;
+import com.example.smarthealthcareappointmentsystem.DTO.PrescriptionDto;
+import com.example.smarthealthcareappointmentsystem.DTO.request.RequestPrescriptionDto;
+import com.example.smarthealthcareappointmentsystem.service.DoctorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/doctor")
+@RequiredArgsConstructor
+public class DoctorController {
+    private final DoctorService doctorService;
+
+    @GetMapping("/{doctorId}/appointments")
+    public ResponseEntity<List<AppointmentDto>>
+    getAppointments(@PathVariable("doctorId") Long doctorId){
+        return ResponseEntity.ok(doctorService.getAppointments(doctorId));
+    }
+    @GetMapping("/{doctorId}/appointments/{appointmentId}")
+    public ResponseEntity<AppointmentDto> getAppointmentById(
+            @PathVariable Long doctorId,
+            @PathVariable Long appointmentId) {
+        return ResponseEntity.ok(doctorService.getAppointmentById(doctorId, appointmentId));
+    }
+    @PutMapping("/{doctorId}/appointments/{appointmentId}/complete")
+    public ResponseEntity<AppointmentDto> markAppointmentAsCompleted(
+            @PathVariable Long doctorId,
+            @PathVariable Long appointmentId) {
+        return ResponseEntity.ok(doctorService.markAppointmentAsCompleted(doctorId, appointmentId));
+    }
+    @PostMapping("/prescriptions")
+    public ResponseEntity<PrescriptionDto> addPrescription(@RequestBody RequestPrescriptionDto requestPrescriptionDto) {
+        return ResponseEntity.ok(doctorService.addPrescription(requestPrescriptionDto));
+    }
+    @GetMapping("/{doctorId}/prescriptions")
+    public ResponseEntity<List<PrescriptionDto>> getPrescriptionsByDoctor(@PathVariable Long doctorId) {
+        return ResponseEntity.ok(doctorService.getPrescriptionsByDoctor(doctorId));
+    }
+
+    @GetMapping("/prescriptions/patient/{patientId}")
+    public ResponseEntity<List<PrescriptionDto>> getPrescriptionsByPatient(@PathVariable Long patientId) {
+        return ResponseEntity.ok(doctorService.getPrescriptionsByPatient(patientId));
+    }
+}
