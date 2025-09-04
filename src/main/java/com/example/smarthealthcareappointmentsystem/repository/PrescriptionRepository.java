afa4753ed6110a4074219edf96876448cf96c694
Kareem Al-Qutob <97916953+Kareem-izzat@users.noreply.github.com>
2025-09-04 15:28:54 +0300

    added pres repo

--- src/main/java/com/example/smarthealthcareappointmentsystem/repository/PrescriptionRepository.java
+++ src/main/java/com/example/smarthealthcareappointmentsystem/repository/PrescriptionRepository.java
@@ -2,10 +2,13 @@ package com.example.smarthealthcareappointmentsystem.repository;
 
 import com.example.smarthealthcareappointmentsystem.entites.mongo.Prescription;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface PrescriptionRepository extends MongoRepository<Prescription,String> {
+@Repository
+public interface PrescriptionRepository extends MongoRepository<Prescription, String> {
+
     List<Prescription> findByPatientId(Long patientId);
 
     List<Prescription> findByDoctorId(Long doctorId);
