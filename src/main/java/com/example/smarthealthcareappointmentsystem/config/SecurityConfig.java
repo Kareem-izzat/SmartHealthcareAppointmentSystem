c06a62f12cb32e0f6ad0db7143212da5dda3609e
Kareem Al-Qutob <97916953+Kareem-izzat@users.noreply.github.com>
2025-09-04 14:52:04 +0300

    edit config for testing

--- src/main/java/com/example/smarthealthcareappointmentsystem/config/SecurityConfig.java
+++ src/main/java/com/example/smarthealthcareappointmentsystem/config/SecurityConfig.java
@@ -3,9 +3,14 @@ package com.example.smarthealthcareappointmentsystem.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
@@ -14,4 +19,25 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("Admin123!"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())  // disable CSRF in new style
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()   // allow all requests
+                );
+
+        return http.build();
+    }
+
 }
--- src/main/java/com/example/smarthealthcareappointmentsystem/controller/AdminController.java
+++ src/main/java/com/example/smarthealthcareappointmentsystem/controller/AdminController.java
@@ -16,7 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
-@PreAuthorize("hasRole('ADMIN')")
+
 public class AdminController {
     private final AdminService adminService;
 
