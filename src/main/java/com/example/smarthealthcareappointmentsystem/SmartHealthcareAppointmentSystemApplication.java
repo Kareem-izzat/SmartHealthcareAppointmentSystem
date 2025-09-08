70caff5787ee9fdd03bfad96c310e6d69b29dc14
Kareem Al-Qutob <97916953+Kareem-izzat@users.noreply.github.com>
2025-09-08 09:10:17 +0300

    added logging aspect

--- src/main/java/com/example/smarthealthcareappointmentsystem/SmartHealthcareAppointmentSystemApplication.java
+++ src/main/java/com/example/smarthealthcareappointmentsystem/SmartHealthcareAppointmentSystemApplication.java
@@ -8,10 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class SmartHealthcareAppointmentSystemApplication {
 
     public static void main(String[] args) {
-        System.out.println("hi there");
-        String rawPassword = "Admin123!";
-        String encoded = new BCryptPasswordEncoder().encode(rawPassword);
-        System.out.println(encoded);
+
         SpringApplication.run(SmartHealthcareAppointmentSystemApplication.class, args);
     }
 
